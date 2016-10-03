27dc9601ce7bad7c3dc85ea180a149230426c0cc
root <root@netmgmt610wh01.polk.edu>
2016-10-03 00:34:02 +0000

    changes

--- EDIT_AND_RUNME.sh
+++ EDIT_AND_RUNME.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Load Overrides File for overwriting Environment Variables in this file
 # This will allow git pulls as they will overwrite this file, but not the overrides.env
 if ! [ -f ./overrides.env ]; then
@@ -6,7 +8,7 @@ fi
 . ./overrides.env
 
 # Set Variables
-ES_SERVER="${ES_SERVER:-1.1.1.1}"
-FLUENTD_SERVER="${FLUENTD_SERVER:-1.1.1.1}"
-
+export ES_SERVER="${ES_SERVER:-1.1.1.1}"
+export FLUENTD_SERVER="${FLUENTD_SERVER:-1.1.1.1}"
+export DOCKER_DATA="${DOCKER_DATA:-/opt/es_data}"
 docker-compose "$@"
