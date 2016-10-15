7ed543da9c24260bd2885a31d0b5a9c3a04a5bce
root <root@netmgmt610wh01.polk.edu>
2016-10-15 01:33:32 +0000

    changes

--- EDIT_AND_RUNME.sh
+++ EDIT_AND_RUNME.sh
@@ -8,7 +8,8 @@ fi
 . ./overrides.env
 
 # Set Variables
-export ES_SERVER="${ES_SERVER:-1.1.1.1}"
+export ES_SERVER1="${ES_SERVER1:-1.1.1.1}"
+export ES_SERVER2="${ES_SERVER2:-1.1.1.1}"
 export FLUENTD_SERVER="${FLUENTD_SERVER:-1.1.1.1}"
 export DOCKER_DATA="${DOCKER_DATA:-/opt/docker_data}"
 docker-compose "$@"
